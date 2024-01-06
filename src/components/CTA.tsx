cc2ec0ef7e3b2b31ed106011bb328b95f621d0f6
Marlon042 <127366345+Marlon042@users.noreply.github.com>
2024-01-06 15:53:55 -0600

    changed CTA
    Agregando funcionalidad descargar cv en CTA

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,23 +1,40 @@
 import { Button } from 'react-bootstrap';
 
-//Call To Action
+// Call To Action
 const CTA = () => {
+  const handleDownloadCV = () => {
+    // Cambia 'nombre_del_cv.pdf' por el nombre real de tu archivo PDF
+    const pdfUrl = '/pdf/mi_cv.pdf';
+
+    // Crea un elemento 'a' (enlace) invisible para la descarga
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = 'mi_cv.pdf';
+
+    // Simula un clic en el enlace para iniciar la descarga
+    document.body.appendChild(link);
+    link.click();
+
+    // Elimina el enlace del cuerpo del documento
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="jumbotron">
       <h1>Hire me!</h1>
       <p>
-        I'm available for new projects & more. Let's work together to build something
-        great. 
+        I'm available for new projects & more. Let's work together to build something great.
       </p>
       <p>
-      *This page is currently in development*
+        *This page is currently in development by myself*
       </p>
       <p>
-        <Button variant="primary" href="#contact-me">
-          Contact me
+        <Button variant="primary" onClick={handleDownloadCV}>
+          Download CV
         </Button>
       </p>
     </div>
   );
 };
+
 export default CTA;
