d1c919461c1713fb1e1525ba1e2e74f68bc2acaf
Marlon042 <127366345+Marlon042@users.noreply.github.com>
2024-02-16 10:16:51 -0600

    Fixed
    CV download fixed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9aaad2342dc6443d"; }
+/******/ 		__webpack_require__.h = function() { return "390633730ab32fe8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
